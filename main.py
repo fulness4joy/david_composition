37315d8c14ea61591c571770c4d0e243463c10f8
David <dikdavi25@gmail.com>
2026-05-15 17:36:20 +0300

    Добавили базовую структуру

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#   начало создания
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+
+class CompositionApp(App):
+    def build(self):
+        button1 = Button(text = "Кнопка 1", font_size = "40sp")
+        label1 = Label(text = "Склад", font_size = "40sp")
+
+        box = BoxLayout(orientation = "vertical")
+        box.add_widget(button1)
+        box.add_widget(label1)
+
+        return box
+
+
+app = CompositionApp()
+app.run()
