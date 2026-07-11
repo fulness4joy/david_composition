50d711b955493c5288906b4db1314d0a376c2c28
David <dikdavi25@gmail.com>
2026-07-11 23:08:24 +0300

    Добавил цену товара и обём; Добавил CheckBox; Добавил екнан Корзина; Добавил кнопку назад в меню

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Color, RoundedRectangle
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.metrics import dp
-from kivy.core.window import Window 
+from kivy.core.window import Window
 from kivy.properties import StringProperty
+from kivy.uix.checkbox import CheckBox
+
 
 
 
@@ -25,8 +28,8 @@ class BoxRow(BoxLayout):
             product_screen = app.root.get_screen('product_screen')
             product_screen.load_product(self.product_name)
             app.root.current = 'product_screen'
-        return super().on_touch_up(touch) 
-        
+        return super().on_touch_up(touch)
+
 
 class MainLabel(Label):
     pass
@@ -36,10 +39,37 @@ class MainImage(Image):
     pass
 
 
+class BasketScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def load_basket(self):
+        screen = self.manager.get_screen("product_screen")
+        products = screen.read_products()
+        self.ids.basket_container.clear_widgets()
+
+        for name in basket:
+            if name in products:
+                info = products[name]
+                bl = BoxLayout(size_hint_y = None, height = dp(50), spacing = '10dp')
+                bl.add_widget(MainLabel(text = name))
+                bl.add_widget(MainLabel(text = info["quantity"]))
+                bl.add_widget(MainLabel(text = info["price"]))
+                bl.add_widget(MainLabel(text = info["volume"]))
+                bl.add_widget(MainImage(source = RESOURCES[name]))
+                self.ids.basket_container.add_widget(bl)
+
+    def goto_main(self):
+        self.manager.current = "main"
+        self.manager.transition.direction = "up"
+
+
 class ProductScreen(Screen):
     product_name = StringProperty("")
     product_image = StringProperty("")
     product_quantity = StringProperty("0")
+    product_price = StringProperty("")
+    product_volume = StringProperty("")
 
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -47,38 +77,33 @@ class ProductScreen(Screen):
     def read_products(self):
         with open(PATH_DATA + "list_products.json", "r", encoding="utf8") as file:
             data = json.load(file)
-
-        products = []
-        for name in data:
-            weight = data[name]
-            products.append((name, weight))
-        return products
+        return data
 
     def write_products(self, products):
-        data = {name: weight for name, weight in products}
         with open(PATH_DATA + "list_products.json", "w", encoding="utf8") as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
+            json.dump(products, file, ensure_ascii = False, indent = 4)
 
     def load_product(self, name):
+        products = self.read_products()
+
         self.product_name = name
-        self.product_image = RESOURCES[name]
+        self.product_image = RESOURCES[self.product_name]
         self.product_quantity = "0"
-        for prod_name, weight in self.read_products():
-            if prod_name == name:
-                self.product_quantity = weight
-                break
+        self.product_price = ""
+        self.product_volume = ""
+
+        if name in products:
+            info = products[name]
+            self.product_quantity = info["quantity"]
+            self.product_price = info["price"]
+            self.product_volume = info["volume"]
 
     def save_quantity(self):
         products = self.read_products()
-        new_products = []
 
-        for name, weight in products:
-            if name == self.product_name:
-                new_products.append((name, self.product_quantity))
-            else:
-                new_products.append((name, weight))
+        if self.product_name in products:
+            products[self.product_name]["quantity"] = self.product_quantity
 
-        products = new_products
         self.write_products(products)
 
     def add_quantity(self):
@@ -90,7 +115,7 @@ class ProductScreen(Screen):
 
     def delete_quantity(self):
         quantity = int(self.product_quantity)
-        if quantity >= 1: 
+        if quantity >= 1:
             quantity -= 1
 
             self.product_quantity = str(quantity)
@@ -98,13 +123,14 @@ class ProductScreen(Screen):
 
     def delete_product(self):
         products = self.read_products()
-        new_products = []
 
-        for name, weight in products:
-            if name != self.product_name:
-                new_products.append((name, weight))
+        if self.product_name in products:
+            del products[self.product_name]
 
-        self.write_products(new_products)
+        self.write_products(products)
+
+        if self.product_name in basket:
+            basket.remove(self.product_name)
 
         self.goto_main()
 
@@ -116,7 +142,7 @@ class ProductScreen(Screen):
 class MenuScreen(Screen):
     def goto_main(self):
         self.manager.current = "main"
-        self.manager.transition.direction = "down"
+        self.manager.transition.direction = "up"
 
 
 class MainScreen(Screen):
@@ -125,25 +151,43 @@ class MainScreen(Screen):
         self.load_resources()
 
     def load_resources(self):
-        with open(PATH_DATA + "list_resources.json", "r", encoding="utf8") as file:
+        with open(PATH_DATA + "list_resources.json", "r", encoding = "utf8") as file:
             data = json.load(file)
-
-        RESOURCES.update(data)
+            RESOURCES.update(data)
 
     def load_products(self):
         screen = self.manager.get_screen("product_screen")
+        products = screen.read_products()
         self.ids.main_container.clear_widgets()
 
-        for product, weight in screen.read_products():
-            bl = BoxRow(size_hint_y = None, height = dp(50), spacing = '10dp', product_name = product)
-            bl.add_widget(MainLabel(text = product))
-            bl.add_widget(MainLabel(text = weight))
-            bl.add_widget(MainImage(source = RESOURCES[product]))
+        for name in products:
+            quantity = products[name]["quantity"]
+            price = products[name]["price"]
+            volume = products[name]["volume"]
 
+            bl = BoxRow(size_hint_y = None, height = dp(50), spacing = '10dp', product_name = name)
+            if name in basket:
+                checkbox = CheckBox(active = True)
+            else:
+                checkbox = CheckBox(active = False)
+            checkbox.bind(active = lambda checkbox, value, name = name: self.on_checkbox_active(checkbox, value, name))
+            
+            bl.add_widget(MainLabel(text = name))
+            bl.add_widget(MainLabel(text = quantity))
+            bl.add_widget(MainLabel(text = price))
+            bl.add_widget(MainLabel(text = volume))
+            bl.add_widget(MainImage(source = RESOURCES[name]))
+            bl.add_widget(checkbox)
             self.ids.main_container.add_widget(bl)
 
-    def reload_products(self):
-        self.load_products()
+    def on_checkbox_active(self, checkbox, value, name):
+        if value:
+            if name not in basket:
+                basket.append(name)
+        else:
+            if name in basket:
+                basket.remove(name)
+
 
     def on_pre_enter(self, *args):
         self.load_products()
@@ -152,7 +196,14 @@ class MainScreen(Screen):
     def goto_main(self):
         self.manager.current = "menu"
         self.manager.transition.direction = "up"
-        
+
+    def goto_basket(self):
+        screen = self.manager.get_screen("basket")
+        screen.load_basket()
+        self.manager.current = "basket"
+        self.manager.transition.direction = "down"
+
+
 
 class CompositionApp(App):
     resources = RESOURCES
@@ -161,7 +212,7 @@ class CompositionApp(App):
         scr_sm.add_widget(MenuScreen(name = "menu"))
         scr_sm.add_widget(MainScreen(name = "main"))
         scr_sm.add_widget(ProductScreen(name = "product_screen"))
-
+        scr_sm.add_widget(BasketScreen(name = "basket"))
 
         return scr_sm
 
--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ import os
 
 PATH = os.path.dirname(__file__) + os.sep
 PATH_DATA = PATH + "data" + os.sep
+global basket
+basket = []
